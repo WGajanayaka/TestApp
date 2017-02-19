a42b6d920bb6e7de1573c3d398f470bcc0b4909e
Wasa <wasa@gpbrands.eu>
2017-02-19 20:21:47 +0530

    Added new file

---
+++ Solution/SchoolHealthManagement/Models/SupplierPaymentSummary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchoolHealthManagement.Models
+{
+    public class SupplierPaymentSummary
+    {
+        public string Province;
+        public int Year;
+        public string Month;
+        public Decimal TotalAmount;
+    }
+}
